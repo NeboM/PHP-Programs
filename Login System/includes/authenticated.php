03a911a4c17314b3bff77c3533b2a140ca8f31da
NeboM <nebbo.o.97@gmail.com>
2019-05-22 17:32:32 +0200

    Update authenticated.php

--- Login System/includes/authenticated.php
+++ Login System/includes/authenticated.php
@@ -1,5 +1,5 @@
 <?php
-// If the user is authenticated redirect to the login page
+// If the user is not authenticated redirect to the login page
 if(empty($_SESSION['user_id'])){
     header('Location: ../public/login.php');
 }
